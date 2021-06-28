1ab6a2ca5f81574bf1e23d3fdffa1f1ac4525cf5
marycatman <marycatman@gmail.com>
2021-06-29 00:38:03 +0300

    Training loop with SGD optimizer and cuda device, overfitting example.

--- CartPoleTrain.py
+++ CartPoleTrain.py
@@ -10,18 +10,19 @@ from torch import from_numpy, as_tensor, float32
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-learning_rate = 0.001
-eps = 0.05
+learning_rate = 0.0005
+eps_0 = 0.9
+eps_N = 0.01
 gamma = 0.999
-episode_count = 1000
+episode_count = 600
 
 
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(4, 5),
-            nn.Linear(5, 2),
+            nn.Linear(4, 4),
+            nn.Linear(4, 2),
         )
 
     # x could be a single state or a batch
@@ -31,15 +32,16 @@ class DQN(nn.Module):
 
 class Agent(object):
 
-    def __init__(self, action_space, model, eps):
+    def __init__(self, action_space, model, eps_0, esp_N, N):
         self.action_space = action_space
         self.model = model
-        self.eps = eps
+        n = np.arange(0, N)
+        self.eps = eps_0 - (eps_0 - eps_N) / N * n
 
-    def get_action(self, state):
+    def get_action(self, state, episode):
         with torch.no_grad():
             q_values = self.model(state)
-            if np.random.uniform() < self.eps:
+            if np.random.uniform() < self.eps[episode]:
                 return self.action_space.sample(), q_values
             else:
                 action = q_values.argmax()
@@ -62,15 +64,14 @@ def main():
     env = gym.make('CartPole-v0')
     env = wrappers.Monitor(env, directory='./tmp', force=True)
 
-    agent = Agent(env.action_space, model, eps)
+    agent = Agent(env.action_space, model, eps_0, eps_N, episode_count)
     episode_durations = []
 
     for i in range(episode_count):
         state = to_tensor(env.reset())
         for t in count():
-            env.render()
-
-            action, q_values = agent.get_action(state)
+            # env.render()
+            action, q_values = agent.get_action(state, i)
             next_state, reward, done, _ = env.step(action)
 
             # Compute prediction error
@@ -90,9 +91,15 @@ def main():
 
     # Close the env and write monitor result info to disk
     env.close()
-    plt.plot(episode_durations)
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
+
+    fig, (ax0, ax1) = plt.subplots(1, 2)
+    ax0.plot(episode_durations)
+    ax0.set_xlabel('Episode')
+    ax0.set_ylabel('Duration')
+    ax1.plot(agent.eps, color='r')
+    ax1.set_xlabel('Episode')
+    ax1.set_ylabel('Epsilon')
+    plt.tight_layout()
     plt.savefig('./tmp/training.png')
     plt.show()
 
