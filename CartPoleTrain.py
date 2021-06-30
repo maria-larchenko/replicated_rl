92f9debbd25906f8ba2b51035f3b51163ec4c22e
marycatman <marycatman@gmail.com>
2021-06-30 18:47:48 +0300

    ReplayMemory class is added, bugs fixed.

--- CartPoleTrain.py
+++ CartPoleTrain.py
@@ -1,3 +1,5 @@
+import os
+
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,28 +9,27 @@ import random
 
 from collections import namedtuple, deque
 from itertools import count
+from tqdm import tqdm
 from gym import wrappers
 from torch import from_numpy, as_tensor, float32, int64
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-learning_rate = 0.0005
-eps_0 = 1.0
-eps_N = 0.1
-gamma = 0.9
-episode_count = 1000
-batch_size = 10
+learning_rate = 0.1
+eps_0 = 0.9
+eps_N = 0.001
+gamma = 0.99
+episode_count = 2000
+batch_size = 50
 
 
 class DQN(nn.Module):
-    def __init__(self, hidden1=6, hidden2=4):
+    def __init__(self):
         super(DQN, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(4, hidden1),
+            nn.Linear(4, 30),
             nn.ReLU(),
-            nn.Linear(hidden1, hidden2),
-            nn.ReLU(),
-            nn.Linear(hidden2, 2),
+            nn.Linear(30, 2),
         )
 
     # x could be a single state or a batch
@@ -49,17 +50,16 @@ class Agent:
             if np.random.uniform() < self.eps[episode]:
                 return self.action_space.sample()
             else:
-                action = self.model(state).argmax()
-                return action.item()
+                return self.model(state).argmax().item()
 
 
 Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward', 'non_final'))
 
 
 class ReplayMemory:
-    """Taken from https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html"""
+    """From https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html"""
 
-    def __init__(self, capacity=2000):
+    def __init__(self, capacity=10000):
         self.memory = deque([], maxlen=capacity)
 
     def push(self, *args):
@@ -71,6 +71,9 @@ class ReplayMemory:
         # see https://stackoverflow.com/a/19343/3343043.
         return Transition(*zip(*transitions))
 
+    def __len__(self):
+        return len(self.memory)
+
 
 def to_tensor(x, dtype=float32):
     return as_tensor(x, dtype=dtype).to(device)
@@ -80,32 +83,27 @@ def main():
     model = DQN().to(device)
     memory = ReplayMemory()
     mse_loss = nn.MSELoss()
-    huber_loss = nn.SmoothL1Loss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
+    
+    weights = sum(p.numel() for p in model.parameters())
     print(f'Using {device} device: {torch.cuda.get_device_name(device=device)}')
-    print(model)
+    print(f'{weights} weights, model: {model}')
 
     env = gym.make('CartPole-v0')
     env = wrappers.Monitor(env, directory='./tmp', force=True)
-
     agent = Agent(env.action_space, model, eps_0, eps_N, episode_count)
     episode_durations = []
 
-    for i in range(episode_count):
-        # state = to_tensor(env.reset())
+    for episode in tqdm(range(episode_count)):
         state = env.reset()
         for t in count():
             # env.render()
-            action = agent.get_action(state, i)
+            action = agent.get_action(state, episode)
             next_state, reward, final, _ = env.step(action)
-
             memory.push(state, action, next_state, reward, int(not final))
-
             state = next_state
 
-            # Experience Replay
-            if t > batch_size:
+            if len(memory) > batch_size:
                 batch = memory.sample(batch_size)
                 states = to_tensor(batch.state)
                 actions = to_tensor(batch.action, dtype=int64)
@@ -113,20 +111,17 @@ def main():
                 next_states = to_tensor(batch.next_state)
                 non_final = to_tensor(batch.non_final)
 
-                q_values = model(states)
-                q_values = q_values.gather(0, actions)
-                q_update = reward + non_final * gamma * model(next_state).max()
+                # Update = r,  for final s'
+                #          r + gamma * max_a Q(s', :), otherwise
+                indices = torch.stack((actions, actions))
+                q_values = model(states).t().gather(0, indices)[0]
+                q_update = rewards + non_final * gamma * model(next_states).amax(dim=1)
+                loss = mse_loss(q_update, q_values)
 
-            # next_state, reward = to_tensor(next_state), to_tensor(reward)
-            # q_update = reward.broadcast_to(2).requires_grad_()
-            # if not final:
-            #     q_update = q_update + gamma * model(next_state).max()
-            # loss = huber_loss(q_values, q_update)
-
-            # Backpropagation
-            # optimizer.zero_grad()
-            # loss.backward()
-            # optimizer.step()
+                # Backpropagation
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
             if final:
                 episode_durations.append(t + 1)
@@ -135,13 +130,13 @@ def main():
     # Close the env and write monitor result to disk
     env.close()
     fig, (ax0, ax1) = plt.subplots(1, 2)
+    fig.suptitle(f'{weights} weights, batch: {batch_size}, lr: {learning_rate}, gamma: {gamma}')
     ax0.plot(episode_durations)
-    ax0.set_xlabel('Episode')
-    ax0.set_ylabel('Duration')
+    ax0.set(xlabel='Episode', ylabel='Duration')
     ax1.plot(agent.eps, color='r')
-    ax1.set_xlabel('Episode')
-    ax1.set_ylabel('Epsilon')
+    ax1.set(xlabel='Episode', ylabel='Epsilon', yscale='log')
     plt.tight_layout()
+    os.replace('./tmp/training.png', './tmp/training_previous.png')
     plt.savefig('./tmp/training.png')
     plt.show()
 
