310d17d9a7fa9980fd46c15b5173ad9bbfb211d0
marycatman <marycatman@gmail.com>
2021-08-20 20:21:56 +0300

    Distributed synchronous version

--- drawing.py
+++ drawing.py
@@ -13,7 +13,7 @@ plt.rcParams['grid.linestyle'] = ':'
 plt.rcParams['grid.linewidth'] = 0.6
 
 
-def plot_results(score, epsilon, title, info, filename, mean_window=100):
+def plot_result(score, epsilon, title, info, filename, mean_window=100):
     running_average = np.convolve(score, np.ones(mean_window)/mean_window, mode='valid')
 
     fig, (ax0, ax1) = plt.subplots(1, 2)
@@ -22,6 +22,27 @@ def plot_results(score, epsilon, title, info, filename, mean_window=100):
     ax0.plot(score)
     ax0.plot(running_average)
     ax0.set(xlabel='Episode', ylabel='Score')
+
+    ax1.plot(epsilon, color='r')
+    ax1.set(xlabel='Episode', ylabel='Epsilon', yscale='log')
+    ax1.text(0.9, 0.9, info, va='top', ha='right', transform=ax1.transAxes)
+
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.show()
+
+
+def plot_results(scores, epsilon, title, info, filename, mean_window=100):
+    fig, (ax0, ax1) = plt.subplots(1, 2)
+    fig.suptitle(title)
+
+    ax0.set(xlabel='Episode', ylabel='Score')
+    for score in scores:
+        ax0.plot(score, alpha=0.7)
+
+    for score in scores:
+        running_average = np.convolve(score, np.ones(mean_window)/mean_window, mode='valid')
+        ax0.plot(running_average, 'r')
 
     ax1.plot(epsilon, color='r')
     ax1.set(xlabel='Episode', ylabel='Epsilon', yscale='log')
